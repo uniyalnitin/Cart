9aaae86b33b0f1c857bf6f645c16d248334c37a8
uniyalnitin <nitin.eee14@nituk.ac.in>
2020-06-14 14:39:24 +0530

    modify state

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,39 +1,77 @@
-import React from 'react'
+import React from "react";
 
-class CartItem extends React.Component{
-    render(){
-        return (
-            <div style={styles.cartItem} className="cart-item">
-                <div className="left-block">
-                    <img style={styles.image}/>
-                </div>
-                <div style={styles.rightBlock} className="right-block">
-                    <div style={{fontSize: 20, fontWeight: 500, fontFamily: "sans-serif"}}>Phone</div>
-                    <div style={{color:"#333", fontStyle:"italic"}}>>Rs 999</div>
-                    <div style={{color:"#333", fontStyle: "oblique"}}>Qty: 1</div>
-                    <div className="cart-item-actions">
-                        {/* Buttons */}
-                    </div>
-                </div>
-            </div>
-        );
-    }
+class CartItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "Mobile Phone",
+      price: 999,
+      qty: 1,
+    };
+  }
+
+  render() {
+    // object destructuring
+    const { title, price, qty } = this.state;
+    return (
+      <div style={styles.cartItem} className="cart-item">
+        <div className="left-block">
+          <img style={styles.image} />
+        </div>
+        <div style={styles.rightBlock} className="right-block">
+          <div
+            style={{ fontSize: 20, fontWeight: 500, fontFamily: "sans-serif" }}
+          >
+            {title}
+          </div>
+          <div style={{ color: "#333", fontStyle: "italic" }}>>Rs {price}</div>
+          <div style={{ color: "#333", fontStyle: "oblique" }}>Qty: {qty}</div>
+          <div style={{marginTop: 10}} className="cart-item-actions">
+              <img style={styles.actionIcons} alt="increase" src="https://image.flaticon.com/icons/svg/992/992651.svg" onClick={this.increaseQty}/>
+              <img style={styles.actionIcons} alt="decrease" src="https://image.flaticon.com/icons/svg/864/864373.svg" onClick={this.decreaseQty}/>
+              <img style={styles.actionIcons} alt="delete" src="https://image.flaticon.com/icons/svg/1345/1345823.svg"/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  increaseQty = () => {
+      this.state.qty += 1;
+      this.setState({
+        any: this.state.qty + 1
+      })
+      console.log(this.state.qty)
+  }
+
+  decreaseQty = () => {
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty - 1
+      }
+    });
+  }
 }
 
 const styles = {
-    image: {
-        height: 100,
-        width: 100,
-        background: "#ccc",
-        borderRadius: 20,
-        border:"none"
-    },
-    cartItem:{
-        display:"flex",
-        alignItems: "center"
-    },
-    rightBlock:{
-        marginLeft: 20,
-    }
-}
+  image: {
+    height: 100,
+    width: 100,
+    background: "#ccc",
+    borderRadius: 20,
+    border: "none",
+  },
+  cartItem: {
+    display: "flex",
+    alignItems: "center",
+  },
+  rightBlock: {
+    marginLeft: 20,
+  },
+  actionIcons:{
+      height: 25,
+      width: 25,
+      marginLeft: 10
+  }
+};
 export default CartItem;
